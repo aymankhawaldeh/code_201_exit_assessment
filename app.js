c44f464eff6c83d2954c8cdb5703f0ff9f0eec80
Ayman <aymankhawaldeh8@gmail.com>
2019-12-04 12:41:10 +0200

    finish

---
+++ app.js
@@ -0,0 +1,17 @@
+
+    var form = document.getElementById("form");
+    var footer = document.getElementById("footer");
+    
+    var ulEl = document.createElement("ul");
+    form.appendChild("ulEl");
+    var liEl = document.createElement("li");
+    ulEl.appendChild("li")
+    
+
+form.addEventListener("submit" , function(e) {
+    console.log(e);
+    e.preventDefault();   
+    
+    
+    
+});
